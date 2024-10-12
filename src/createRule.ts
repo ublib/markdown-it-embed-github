1f0dbe0b743e8ef4b41b50d9f04605850a69f8fc
Ubugeeei <ubuge1122@gmail.com>
2024-10-13 00:34:33 +0900

    refactor: rules

---
+++ src/createRule.ts
@@ -0,0 +1,39 @@
+import type MarkdownIt from "markdown-it";
+
+import type { Options } from ".";
+import { fetchHtmlSync } from "./fetch";
+import { parse } from "./parse";
+import { renderToHtml } from "./render";
+import { isGithubUrl } from "./utils";
+
+export function createRule(options: Options | undefined, md: MarkdownIt): void {
+  md.block.ruler.before(
+    "paragraph",
+    "github_link_block",
+    (state, startLine) => {
+      const line = state
+        .getLines(startLine, startLine + 1, state.blkIndent, false)
+        .trim();
+
+      if (!isGithubUrl(line)) {
+        return false;
+      }
+
+      const token = state.push("github_link", "", 0);
+      token.content = line;
+      token.map = [startLine, startLine + 1];
+
+      state.line = startLine + 1;
+
+      return true;
+    },
+  );
+
+  md.renderer.rules.github_link = (tokens, idx) => {
+    const c = tokens[idx].content;
+    const rawHtml = fetchHtmlSync(c);
+    const parsed = parse(rawHtml, c);
+    const res = (options?.renderer ? options.renderer : renderToHtml)(parsed);
+    return res;
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import type { PluginSimple } from "markdown-it";
 
 import type { GitHubSourceDescriptor } from "./parse";
-import { fetchHtmlSync } from "./fetch";
-import { parse } from "./parse";
-import { renderToHtml } from "./render";
-import { isGithubUrl } from "./utils";
+import { createRule } from "./createRule";
 
 export interface Options {
   renderer?: (descriptor: GitHubSourceDescriptor) => string;
@@ -35,35 +32,7 @@ type Plugin = (options?: Options) => PluginSimple;
  * ```
  */
 const previewGitHubSource: Plugin = options => (md) => {
-  md.block.ruler.before(
-    "paragraph",
-    "github_link_block",
-    (state, startLine) => {
-      const line = state
-        .getLines(startLine, startLine + 1, state.blkIndent, false)
-        .trim();
-
-      if (!isGithubUrl(line)) {
-        return false;
-      }
-
-      const token = state.push("github_link", "", 0);
-      token.content = line;
-      token.map = [startLine, startLine + 1];
-
-      state.line = startLine + 1;
-
-      return true;
-    },
-  );
-
-  md.renderer.rules.github_link = (tokens, idx) => {
-    const c = tokens[idx].content;
-    const rawHtml = fetchHtmlSync(c);
-    const parsed = parse(rawHtml, c);
-    const res = (options?.renderer ? options.renderer : renderToHtml)(parsed);
-    return res;
-  };
+  createRule(options, md);
 };
 
 export default previewGitHubSource;
